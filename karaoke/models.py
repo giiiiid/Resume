cb65e5ef47ed0ce08db0ddbcb0928647f44ee7b9
Giiid <gideonfummey1@gmail.com>
2023-10-05 15:30:21 +0200

    A model to add sogs

--- karaoke/models.py
+++ karaoke/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Authentication(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Song(models.Model):
+    artiste = models.CharField(max_length=150)
+    song_name = models.CharField(max_length=150)
+    cover_image = models.ImageField(upload_to='cover_images')
+    audio_file = models.FileField(upload_to='audios')
+
+    def __str__(self):
+        return self.artiste + '-' + self.song_name
